790abd91aa95392116ca23eaa43f23fe1b2ac3da
Caio Mognatti <caiocsm97@gmail.com>
2025-01-31 04:03:22 -0300

    feat: adds a get into task route index and exports it

--- apps/api/src/routes/tasks.routes.ts
+++ apps/api/src/routes/tasks.routes.ts
@@ -3,4 +3,10 @@ import { createTask } from "../controllers/task.controller";
 
 const taskRoutes = Router();
 
+taskRoutes.get("/", (req, res) => {
+  res.status(200).send("Base para a pesquisa e manipulação de tarefas");
+});
+
 taskRoutes.post("/", createTask);
+
+export { taskRoutes };
