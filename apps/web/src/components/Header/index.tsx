1de8d13c17574dd2a072781e170870958c7d48e9
Caio Mognatti <caiocsm97@gmail.com>
2025-01-31 02:23:32 -0300

    feat: creates a base for the header component

---
+++ apps/web/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styles";
+
+export default function Header() {
+  return (
+    <S.HeaderContainer>
+      <S.HeaderNav>
+        Mognatti - Task Manager
+        <button>sair</button>
+      </S.HeaderNav>
+    </S.HeaderContainer>
+  );
+}
