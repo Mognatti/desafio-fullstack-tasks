df54e61f6f905d92587cb488ff2437fe2ef41474
Caio Mognatti <caiocsm97@gmail.com>
2025-01-30 23:33:49 -0300

    feat: uses the new auth routes to allow login

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,18 @@
+import { userRoutes } from "./routes/user.routes";
+import { authRoute } from "./routes/auth.routes";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { userRoutes } from "./routes/user.routes";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? 8080;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoutes);
 
 app.listen(port, () => console.log(`API rodando em: ${port}`));
