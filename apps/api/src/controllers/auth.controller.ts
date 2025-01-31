9f28c8fe0395bf9af5f04fda5f510ad5f6b8c89b
Caio Mognatti <caiocsm97@gmail.com>
2025-01-31 12:35:46 -0300

    feat: creates the register new user function on the api

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -8,7 +8,17 @@ export async function login(req: Request, res: Response) {
     const { email, password } = req.body;
     const { user, token } = await authService.login({ email, password });
     res.status(200).json({ user, token });
-  } catch (error) {
-    res.status(401).json(error);
+  } catch (error: any) {
+    res.status(401).json({ status: error.status ?? 401, message: error.message ?? "Usuário ou senha inválidos" });
+  }
+}
+
+export async function register(req: Request, res: Response) {
+  try {
+    const { name, email, password } = req.body;
+    const { user, token } = await authService.register(name, email, password);
+    res.status(201).json({ user, token });
+  } catch (error: any) {
+    res.status(400).json({ status: error.status ?? 400, message: error.message ?? "Erro ao criar usuário" });
   }
 }
--- apps/api/src/services/auth.service.ts
+++ apps/api/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "../../utils/class.error";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
-import { compare } from "bcrypt-ts";
+import { compare, hash } from "bcrypt-ts";
 
 const prisma = new PrismaClient();
 
@@ -14,29 +14,66 @@ const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN ?? "1h";
 
 export default class AuthService {
   async login({ email, password }: LoginProps) {
-    const user = await prisma.user.findUnique({ where: { email }, include: { tasks: true } });
-
-    if (!user) {
-      throw new CustomError("Usuário não encontrado", 404);
+    if (!email || !password) {
+      throw new CustomError("Email e senha obrigatórios", 400);
     }
+    try {
+      const user = await prisma.user.findUnique({ where: { email }, include: { tasks: true } });
 
-    const checkPassword = await compare(password, user.password);
-    if (!checkPassword) {
-      throw new CustomError("Senha incorreta", 401);
+      if (!user) {
+        throw new CustomError("Usuário não encontrado", 404);
+      }
+
+      const checkPassword = await compare(password, user.password);
+      if (!checkPassword) {
+        throw new CustomError("Senha incorreta", 401);
+      }
+
+      if (!JWT_SECRET) {
+        return { user: "Problema no import do JWT_SECRET, arrumar futuramente", token: null };
+      }
+
+      const token = jwt.sign({ id: user.id }, JWT_SECRET);
+
+      const userData = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        tasks: user.tasks,
+      };
+      return { user: userData, token };
+    } catch (error: any) {
+      if (error instanceof CustomError) {
+        throw new CustomError("Erro ao fazer login", 500);
+      } else {
+        throw error;
+      }
     }
+  }
 
-    if (!JWT_SECRET) {
-      return { user: "Problema no import do JWT_SECRET, arrumar futuramente", token: null };
+  async register(name: string, email: string, password: string) {
+    if (!name || !email || !password) {
+      throw new CustomError("Nome, email e senha obrigatórias", 400);
     }
-
-    const token = jwt.sign({ id: user.id }, JWT_SECRET);
-
-    const userData = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      tasks: user.tasks,
-    };
-    return { user: userData, token };
+    try {
+      const hashedPassword = await hash(password, 10);
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+      if (user) {
+        return this.login({ email, password });
+      }
+      throw new CustomError("Erro ao criar usuário", 500);
+    } catch (error: any) {
+      if (error instanceof CustomError) {
+        throw new CustomError("Erro ao criar usuário", 500);
+      } else {
+        throw error;
+      }
+    }
   }
 }
